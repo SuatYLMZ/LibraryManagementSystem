6c7c66ea2e1fc62301ba6f8ee7cb2af1731916fd
SuatYLMZ <furkansuatylmz@gmail.com>
2023-07-17 22:58:19 -0700

    first commit

---
+++ src/main/java/com/suatylmz/librarymanagementsystem/Entity.java
@@ -0,0 +1,2 @@
+package com.suatylmz.librarymanagementsystem;public class Entity {
+}
---
+++ src/test/java/com/suatylmz/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suatylmz.librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
